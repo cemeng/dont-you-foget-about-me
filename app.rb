56a16764ada5ddc6899b3acc7800ad2c4c3589b7
Felix Tjandrawibawa <ftjandrawibawa@scentregroup.com>
2021-01-26 00:06:54 +1100

    intial setup

---
+++ app.rb
@@ -0,0 +1,15 @@
+require "functions_framework"
+require "cgi"
+require "json"
+
+FunctionsFramework.http "hello_http" do |request|
+  # The request parameter is a Rack::Request object.
+  # See https://www.rubydoc.info/gems/rack/Rack/Request
+  name = request.params["name"] ||
+         (JSON.parse(request.body.read)["name"] rescue nil) ||
+         "World"
+  # Return the response body as a string.
+  # You can also return a Rack::Response object, a Rack response array, or
+  # a hash which will be JSON-encoded into a response.
+  "Hello #{CGI.escape_html name}!"
+end
